7ec682b98daef77c266de39af4ff47d472764b6f
YafimK <yafim.kazak@gmail.com>
2018-01-15 09:56:10 +0200

    Basic server impl based on express and https module initial dev

--- https_server.js
+++ https_server.js
@@ -5,7 +5,8 @@ var program_cmd_args = require('commander');
 const https = require("https");
 const fs = require('fs');
 const qs = require('querystring');
-var express = require("express");
+const express = require("express");
+const errorHandler = require('errorhandler'),
 
 
 //Parse cmd args
@@ -16,10 +17,29 @@ program_cmd_args
     .option('-H, --host', 'Host address binding', 'localhost')
     .option('-C, --cert', 'Certificate', 'unsafe_default_server_key.cert')
     .option('-K, --key', 'Private key', 'unsafe_default_server_key.key')
+    .option('-SF, --publish-folder', 'static folder to server pages from', 'public')
     .parse(process.argv);
-
 
 const https_server_options = {
     key: fs.readFileSync(program_cmd_args.key),
     cert: fs.readFileSync(program_cmd_args.cert)
 };
+
+const server_options = {
+    port: program_cmd_args.port,
+    host: program_cmd_args.host
+};
+
+
+const app = express();
+
+if(program_cmd_args.publishFolder){
+    app.use(express.static(program_cmd_args.publishFolder))
+}
+
+app.use(errorHandler({
+    dumpExceptions: true,
+    showStack: true
+}));
+https.createServer(https_server_options, app).listen(server_options);
+console.log('Server Started at: https://',program_cmd_args.host,':', program_cmd_args.port);
